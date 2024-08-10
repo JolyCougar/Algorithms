e0f62a33abbd26f2ab3c1dd80c7d34037970e5f6
danero <danero95@yandex.ru>
2024-08-10 17:35:53 +0300

    leetcode task

---
+++ leetcode_page2/Binary_Tree_Paths.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        result, path = [], []
+        self.binaryTreePathsRecu(root, path, result)
+        return result
+
+    def binaryTreePathsRecu(self, node, path, result):
+        if node is None:
+            return
+
+        if node.left is node.right is None:
+            ans = ""
+            for n in path:
+                ans += str(n.val) + "->"
+            result.append(ans + str(node.val))
+
+        if node.left:
+            path.append(node)
+            self.binaryTreePathsRecu(node.left, path, result)
+            path.pop()
+
+        if node.right:
+            path.append(node)
+            self.binaryTreePathsRecu(node.right, path, result)
+            path.pop()
