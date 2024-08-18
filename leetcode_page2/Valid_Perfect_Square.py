5f3b6490791de78e2c793631c9b816d4ecae8d97
Yuri <Jolycougar@gmail.com>
2024-08-18 11:58:32 +0300

    leetcode task

--- leetcode_page2/Valid_Perfect_Square.py
+++ leetcode_page2/Valid_Perfect_Square.py
@@ -3,4 +3,17 @@ class Solution:
         return (int(num ** 0.5) ** 2) == num
 
 
+# ---------------------------------------------
+# if num == 1:
+#     return True
+# l, r = 1, (num // 2) + 1
+# while l <= r:
+#     mid = (r + l) // 2
+#     if mid * mid == num:
+#         return True
+#     elif mid * mid < num:
+#         l = mid + 1
+#     else:
+#         r = mid - 1
+# return False
 print(Solution().isPerfectSquare(13))
