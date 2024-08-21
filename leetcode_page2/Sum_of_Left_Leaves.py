a0d1044b6302e7ae1fab8eb6816cd09db6664dad
Yuri <Jolycougar@gmail.com>
2024-08-21 21:49:56 +0300

    leetcode task

---
+++ leetcode_page2/Sum_of_Left_Leaves.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        stack = [root]
+        res = 0
+        while stack:
+            node = stack.pop(0)
+            if node.left:
+                if not node.left.left and not node.left.right:
+                    res += node.left.val
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return res
