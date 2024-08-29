deacb91afcf8fd14221c30ad2580c8d93a32ae81
Yuri <Jolycougar@gmail.com>
2024-08-29 21:49:02 +0300

    leetcode task

---
+++ leetcode/22_Generate_Parentheses.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.n = n
+        self.stack = []
+        self.res = []
+
+        self.backtrack(0, 0)
+        return self.res
+
+    def backtrack(self, opened, closed):
+        if opened == closed == self.n:
+            self.res.append("".join(self.stack))
+            return
+
+        if opened < self.n:
+            self.stack.append("(")
+            self.backtrack(opened + 1, closed)
+            self.stack.pop()
+
+        if closed < opened:
+            self.stack.append(")")
+            self.backtrack(opened, closed + 1)
+            self.stack.pop()
+
+
+print(Solution().generateParenthesis(3))
